9fc6ff728710b58cbd6eb1bd27e3c1deb8a2004c
szalazar987 <32672914+szalazar987@users.noreply.github.com>
2018-02-13 12:07:50 +0100

    Create ansible-install.sh

---
+++ ansible-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get insatll ansible
+#sudo apt-get install openssh-server -y
